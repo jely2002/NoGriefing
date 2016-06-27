090e6087a705a7e8cfa77a68997533c766456cdf
jely2002 <jw.glebbeek@gmail.com>
2016-06-27 10:21:10 +0200

    Adding event listener
    Adding event listener to make the managing of events easier.

---
+++ src/jely2002/bukkit/NoGriefers/EventListener.java
@@ -0,0 +1,13 @@
+package jely2002.bukkit.NoGriefers;
+
+import jely2002.bukkit.NoGriefers.Main;
+
+public class EventListener implements org.bukkit.event.Listener {
+
+	Main plugin;
+	
+	public EventListener(Main instance) {
+		plugin = instance;
+		}
+
+}
