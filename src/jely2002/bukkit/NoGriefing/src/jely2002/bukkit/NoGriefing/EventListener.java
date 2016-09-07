acf243f73e1d9d4f31cd3e423fcf07ccea2aa6a7
jely2002 <jw.glebbeek@gmail.com>
2016-09-07 21:03:36 +0200

    Update 1.7

---
+++ src/jely2002/bukkit/NoGriefing/src/jely2002/bukkit/NoGriefing/EventListener.java
@@ -0,0 +1,279 @@
+package jely2002.bukkit.NoGriefing;
+
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+import jely2002.bukkit.NoGriefing.Main;
+
+public class EventListener implements org.bukkit.event.Listener {
+
+	private final ArrayList<Player> cooldown = new ArrayList<Player>();
+
+	Main plugin;
+	public EventListener(Main instance) {
+		plugin = instance;
+	}
+
+
+	@EventHandler
+	public void blockbreak(BlockBreakEvent e) {
+		final Player p = e.getPlayer();
+		if (plugin.getConfig().getBoolean("blockbreak") == false && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getPlayer().getWorld().getName())){
+			if (p.hasPermission("BreakBlocks")) {
+				return;
+			} else {
+				if (!cooldown.contains(p)) {
+					cooldown.add(p);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-blockbreak")));
+					Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							cooldown.remove(p);
+						}
+					}, 30);			  		
+				} 
+				e.setCancelled(true);	
+			}
+		} else return;
+	}
+
+	@EventHandler
+	public void blockplace(BlockPlaceEvent e) {
+		final Player p = e.getPlayer();
+		if (plugin.getConfig().getBoolean("blockplace") == false  && !p.hasPermission("PlaceBlocks") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getPlayer().getWorld().getName())) {
+			if (!cooldown.contains(p)) {
+				cooldown.add(p);
+				e.setCancelled(true);
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-blockplace")));
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						cooldown.remove(p);
+					}
+				}, 30);			  		 
+				e.setCancelled(true);	
+			}
+			e.setCancelled(true);		
+		} else return;
+	}
+
+
+    @EventHandler
+    public void BucketEmpty(PlayerBucketEmptyEvent e) {
+        final Player p = e.getPlayer();
+        if (plugin.getConfig().getBoolean("blockplace") == false && !p.hasPermission("PlaceBlocks") && !plugin.getConfig().getStringList("disabled-worlds").contains(p.getWorld().getName())) {
+    		e.setCancelled(true);
+			if (!cooldown.contains(p)) {
+				cooldown.add(p);
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-blockplace")));
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						cooldown.remove(p);
+					}
+				}, 30);			  
+			}
+    		e.setCancelled(true);
+        }
+    }
+    
+    @EventHandler
+    public void BucketEmpty(PlayerBucketFillEvent e) {
+        final Player p = e.getPlayer();
+        if (plugin.getConfig().getBoolean("blockbreak") == false && !p.hasPermission("BreakBlocks") && !plugin.getConfig().getStringList("disabled-worlds").contains(p.getWorld().getName())) {
+			if (!cooldown.contains(p)) {
+				cooldown.add(p);
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-blockbreak")));
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						cooldown.remove(p);
+					}
+				}, 30);			  
+			}
+    		e.setCancelled(true);
+        }
+    }
+
+
+	@EventHandler
+	public void pickupitems(PlayerPickupItemEvent e) {
+		final Player p = e.getPlayer();
+		if (plugin.getConfig().getBoolean("itempickup") == false && !p.hasPermission("ItemPickup")  && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getPlayer().getWorld().getName())) {
+			if (!cooldown.contains(p)) {
+				cooldown.add(p);
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-itempickup")));
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						cooldown.remove(p);
+					}
+				}, 30);			  
+			}
+			e.setCancelled(true);
+		} else return;
+	}
+
+	@EventHandler
+	public void dropitems(PlayerDropItemEvent e) {
+		final Player p = e.getPlayer();
+		if (plugin.getConfig().getBoolean("itemdrops") == false && !p.hasPermission("ItemDrops")  && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getPlayer().getWorld().getName())) {
+			if (!cooldown.contains(p)) {
+				cooldown.add(p);
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-itemdrops")));
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					public void run() {
+						cooldown.remove(p);
+					}
+				}, 30);				
+			}
+			e.setCancelled(true);
+		} else return;
+	}
+
+	@EventHandler
+	public void antitntevent(EntityExplodeEvent e) {
+		if (plugin.getConfig().getBoolean("antittnt") && e.getEntityType() == EntityType.PRIMED_TNT && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getLocation().getWorld().getName())) {
+			e.setCancelled(true);
+		} else return;
+	}
+
+	@EventHandler
+	public void antitntevent2(ExplosionPrimeEvent e) {
+		if (plugin.getConfig().getBoolean("antitnt") && e.getEntityType() == EntityType.PRIMED_TNT && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getEntity().getLocation().getWorld().getName())) {
+			e.setCancelled(true);
+		} else return;
+	}
+
+
+	@EventHandler
+	public void use(InventoryOpenEvent e) {
+		HumanEntity p = e.getPlayer();
+		if (plugin.getConfig().getBoolean("inventory-use") == false && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getPlayer().getWorld().getName())) {
+			if (!p.hasPermission("use")) {
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-use")));
+				e.setCancelled(true);
+			}
+		} else return;
+	}
+
+	@EventHandler
+	public void blocklogger(BlockPlaceEvent e) {	
+		String blockplaced = e.getBlock().getType().toString();
+		Player placer = e.getPlayer();
+		if (plugin.getConfig().getBoolean("enable-blocklog"))  {	
+			if (plugin.loggedblocks.contains(blockplaced)) {
+				plugin.logger.put(placer, blockplaced);
+			} 
+		}	
+	}
+	
+	@EventHandler
+	public void nodamage(EntityDamageByEntityEvent e) {
+		if (plugin.getConfig().getBoolean("pvp") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getDamager().getLocation().getWorld().getName())) {
+			if (e.getDamager() instanceof Player) {
+				if (!e.getDamager().hasPermission("pvp")) {
+					final Player p = (Player) e.getDamager();
+					if (!cooldown.contains(p)) {
+						cooldown.add(p);
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-pvp").replace("%s", e.getEntity().getName())));
+						Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+							public void run() {
+								cooldown.remove(p);
+							}
+						}, 30);				
+					}
+					e.setCancelled(true);
+				}
+			}
+		} else return;
+	}
+	
+	@EventHandler
+	public void noentityeffects(EntityDamageEvent e) {
+		if (plugin.getConfig().getBoolean("pvp") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getEntity().getLocation().getWorld().getName())) {
+			if (e.getEntity() instanceof Player) {
+				if (!e.getEntity().hasPermission("pvp")) {
+					if (e.getCause() == DamageCause.WITHER || e.getCause() == DamageCause.DRAGON_BREATH || e.getCause() == DamageCause.ENTITY_ATTACK || e.getCause() == DamageCause.ENTITY_EXPLOSION || e.getCause() == DamageCause.MAGIC) {
+					e.setCancelled(true);
+					}
+				}
+			}
+		} else return;
+	}
+	
+	@EventHandler
+	public void noarrowdamage(EntityShootBowEvent e) {
+		if (plugin.getConfig().getBoolean("pvp") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getEntity().getLocation().getWorld().getName())) {
+			if (e.getEntity() instanceof Player) {
+				if (!e.getEntity().hasPermission("pvp")) {
+					final Player p = (Player) e.getEntity();
+					if (!cooldown.contains(p)) {
+						cooldown.add(p);
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("deny-message-bow")));
+						Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+							public void run() {
+								cooldown.remove(p);
+							}
+						}, 30);				
+					}
+					e.setCancelled(true);
+				}
+			}
+		} else return;
+	}
+	
+	@EventHandler
+	public void nofalldamage(EntityDamageEvent e) {
+		if (plugin.getConfig().getBoolean("no-falldamage") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getEntity().getLocation().getWorld().getName())) {
+			if (e.getEntity() instanceof Player) {
+				if (!e.getEntity().hasPermission("fall")) {
+					if (e.getCause() == DamageCause.FALL || e.getCause() == DamageCause.CONTACT) {
+					e.setCancelled(true);
+					}
+				}
+			}
+		} else return;
+	}
+	
+	@EventHandler
+	public void nofireanddrowndamage(EntityDamageEvent e) {
+		if (plugin.getConfig().getBoolean("no-fire-drowning") && !plugin.getConfig().getStringList("disabled-worlds").contains(e.getEntity().getLocation().getWorld().getName())) {
+			if (e.getEntity() instanceof Player) {
+				if (!e.getEntity().hasPermission("fire-water")) {
+					if (e.getCause() == DamageCause.DROWNING || e.getCause() == DamageCause.FIRE || e.getCause() == DamageCause.FIRE_TICK || e.getCause() == DamageCause.LIGHTNING || e.getCause() == DamageCause.HOT_FLOOR || e.getCause() == DamageCause.LAVA) {
+					e.setCancelled(true);
+					}
+				}
+			}
+		} else return;
+	}
+	
+	@EventHandler
+	public void joinupdate(PlayerJoinEvent e) {	
+		Player p = e.getPlayer();
+		if (plugin.uptodate == false && plugin.getConfig().getBoolean("update-check") && p.isOp()) {
+			p.sendMessage(ChatColor.GREEN + "NoGriefing " + ChatColor.GRAY + "A new version of the plugin was found!");
+			p.sendMessage(ChatColor.GREEN + "NoGriefing " + ChatColor.GRAY + "Download it at:");
+			p.sendMessage(ChatColor.GREEN + "NoGriefing " + ChatColor.GRAY + "http://dev.bukkit.org/bukkit-plugins/antigrief-nogriefers/");
+		} else return;
+	}
+}
+
+
---
+++ src/jely2002/bukkit/NoGriefing/src/jely2002/bukkit/NoGriefing/Main.java
@@ -0,0 +1,209 @@
+package jely2002.bukkit.NoGriefing;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import jely2002.bukkit.NoGriefing.Updater.UpdateResult;
+import jely2002.bukkit.NoGriefing.Updater.UpdateType;
+import jely2002.bukkit.NoGriefing.Updater;
+
+public class Main extends JavaPlugin {
+
+	public boolean uptodate;
+	private Menu menu;
+	public HashMap<OfflinePlayer, String> logger = new HashMap<OfflinePlayer, String>();
+	public List<String> loggedblocks = (List<String>) this.getConfig().getStringList("blocks-to-be-logged");
+	Listener listener = new EventListener(this);
+
+	@Override
+	public void onEnable() {
+		menu = new Menu(this);
+		Bukkit.getPluginManager().registerEvents(listener, this);
+		Bukkit.getPluginManager().registerEvents(menu, this);
+		PluginManager pm = getServer().getPluginManager();
+		Permission BreakBlocks = new Permission("ng.breakblocks");
+		Permission DropItems = new Permission("ng.dropitems");
+		Permission PlaceBlocks = new Permission("ng.placeblocks");
+		Permission ChangeState = new Permission("ng.setflags");
+		Permission SosMode = new Permission("ng.sosmode");
+		Permission Reload = new Permission("ng.reload");
+		Permission Blocklog = new Permission("ng.viewblocklog");
+		Permission DelBlocklog = new Permission("ng.delblocklog");
+		Permission use = new Permission("ng.openinventory");
+		Permission PvP = new Permission("ng.pvp");
+		Permission ItemPickup = new Permission("ng.itempickup");
+		pm.addPermission(BreakBlocks);
+		pm.addPermission(use);
+		pm.addPermission(DropItems);
+		pm.addPermission(ItemPickup);
+		pm.addPermission(PlaceBlocks);
+		pm.addPermission(ChangeState);
+		pm.addPermission(SosMode); 
+		pm.addPermission(Reload);
+		pm.addPermission(Blocklog);
+		pm.addPermission(DelBlocklog);
+		pm.addPermission(PvP);
+		FileConfiguration config = this.getConfig();
+		config.options().copyDefaults(true);
+		this.saveConfig();
+		getLogger().info("--------------+ NoGriefing +---------------");
+		getLogger().info("       NoGriefing is succesfully enabled!  ");
+		getLogger().info("        Thank you for using NoGriefing!    ");
+		getLogger().info("       To see the commands do /ng help     ");
+		getLogger().info("         By: Jely2002 - Open source!       ");
+		getLogger().info("--------------+ NoGriefing +---------------");
+
+		//Start metrics
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+			getLogger().warning("Could not connect to MCStats.com");
+		}
+
+		//Start updater
+		if (getConfig().getBoolean("update-check")) {
+			Updater updatechecker = new Updater(this, 100722, getFile(), UpdateType.NO_DOWNLOAD, true);
+			if (getConfig().getBoolean("update-check"))
+			if (updatechecker.getResult().equals(UpdateResult.UPDATE_AVAILABLE)) {
+				getLogger().info("There is an update available!");
+				getLogger().info("Download it now at the plugin page:");
+				getLogger().info("http://dev.bukkit.org/bukkit-plugins/antigrief-nogriefers/");
+				uptodate = false;
+			} else {
+				getLogger().info("You are running the latest version of NoGriefing!");
+				uptodate = true;
+			}
+		}
+
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			public void run() {
+				List<String> blocklist = getConfig().getStringList("blocklog");
+				for (OfflinePlayer p : logger.keySet()) {
+					blocklist.add(p.getName() + ": " + logger.get(p).toString());
+				}
+				getConfig().set("blocklog", blocklist);
+				saveConfig();
+				logger.clear();
+
+			}
+		}, 10*20, getConfig().getInt("blocklog-save-interval")*20);			
+	}
+
+	@Override
+	public void onDisable() {
+		List<String> blocklist = getConfig().getStringList("blocklog");
+		for (OfflinePlayer p : logger.keySet()) {
+			blocklist.add(p.getName() + ": " + logger.get(p).toString());
+		}
+		getConfig().set("blocklog", blocklist);
+		saveConfig();
+		getLogger().info("---+ Thanks for using NoGriefing - Succesfully disabled +---");
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (cmd.getName().equalsIgnoreCase("ng") && sender instanceof Player) {
+
+			int length = args.length;
+			Player p = (Player) sender;
+
+			if (length == 0) {
+				return false;			
+			}
+
+			if (length == 1) {
+
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (p.hasPermission("Reload")) {
+						this.reloadConfig();
+						this.saveConfig();
+						p.sendMessage(ChatColor.GREEN +"Config succesfully reloaded!");
+					} else p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-permission-message")));
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("help")) {
+					p.sendMessage(ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "==========" + " " + ChatColor.GREEN.toString() + ChatColor.BOLD + "NoGriefing" + " " + ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "==========" + "");
+					p.sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD + "/ng help");
+					p.sendMessage(ChatColor.GRAY + "Opens this help menu. ");
+					p.sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD + "/ng sos");
+					p.sendMessage(ChatColor.GRAY + "Activates emergency mode.");
+					p.sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD + "/ng settings");
+					p.sendMessage(ChatColor.GRAY + "Opens a GUI where you can enable or disable all the anti-grief features.");
+					p.sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD + "/ng blocklog");
+					p.sendMessage(ChatColor.GRAY + "Shows the blocklog.");
+					p.sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD + "/ng version");
+					p.sendMessage(ChatColor.GRAY + "Shows the version of the plugin.");
+					p.sendMessage(ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "==========" + " " + ChatColor.GREEN.toString() + ChatColor.BOLD + "By: Jely2002" + " " + ChatColor.DARK_GRAY.toString() + ChatColor.BOLD + "==========" + "");
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("version")) {
+					p.sendMessage(ChatColor.GREEN + "NoGriefing is on version: " + ChatColor.YELLOW + getDescription().getVersion());
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("sos")) {
+					if (p.hasPermission("SosMode")) {
+						this.getConfig().set("blockplace", false); 
+						this.getConfig().set("blockbreak", false); 
+						this.getConfig().set("itemdrops", false);
+						this.getConfig().set("antitnt", true);
+						this.saveConfig();
+						p.sendMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + "Emergency mode activated. All privileges taken.");
+						Bukkit.broadcastMessage(ChatColor.DARK_RED.toString() + ChatColor.BOLD + "Emergency mode activated. Nobody can place, break or drop items.");
+					} else p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-permission-message")));
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("blocklog")) {
+					if (p.hasPermission("Blocklog")) {
+						if (getConfig().getBoolean("enable-blocklog")) {
+							for(int i = 0; i < getConfig().getStringList("blocklog").size() ;  i++)	 {
+								p.sendMessage(ChatColor.GOLD + getConfig().getStringList("blocklog").get(i));
+								p.sendMessage(ChatColor.GREEN + "Blocklog can be emptied using: /ng delblocklog");
+							}
+						} else p.sendMessage(ChatColor.RED + "To use this feature, set enable-blocklog in the config to true.");
+					} else p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-permission-message")));
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("delblocklog")) {
+					if (p.hasPermission("DelBlocklog")) {	
+						if (getConfig().getBoolean("enable-blocklog")) {
+							loggedblocks.clear();
+							logger.clear();
+							getConfig().set("blocklog", null);
+							saveConfig();
+							p.sendMessage(ChatColor.GREEN + "Blocklog cleared!");
+						} else p.sendMessage(ChatColor.RED + "To use this feature, set enable-blocklog in the config to true.");
+					}  else p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-permission-message")));
+					return true;
+				}
+
+				if (args[0].equalsIgnoreCase("settings")) {
+					if (p.hasPermission("ChangeState")) {
+						p.openInventory(Menu.settingsgui);
+					}  else p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("no-permission-message")));
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
+}
